8626f662816fff3e36b3f059c71bf7e57bca2495
ppark2ya <vigabhan1231@gmail.com>
2021-10-24 11:02:41 +0900

    fix: add working directory working directory를 지정하지 않아 CI/CD시 경로 못찾는 문제를 해결
    resolve #8

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
+const path = require("path");
+const { BUCKET_NAME, WEB_CF_DISTRIBUTION_ID } = process.env;
+
 module.exports = {
   reactStrictMode: true,
-}
+  // https://www.serverless.com/plugins/serverless-nextjs-plugin
+  env: {
+    BUCKET_NAME,
+    WEB_CF_DISTRIBUTION_ID,
+  },
+  webpack5: false,
+  webpack: (config, options) => {
+    // alias 추가
+    config.resolve.alias["~"] = path.resolve(__dirname, "src");
+
+    return config;
+  },
+};
