96843a0389dee02f9888d476af0a1336ebfea04c
ppark2ya <vigabhan1231@gmail.com>
2021-10-24 19:36:57 +0900

    chore(deploy): env 추가 serverless 실행 env를 추가하기 위해 web-dev.yml 수정
    ref #8

--- web/next.config.js
+++ web/next.config.js
@@ -1,13 +1,8 @@
 const path = require("path");
-const { BUCKET_NAME, WEB_CF_DISTRIBUTION_ID } = process.env;
 
 module.exports = {
   reactStrictMode: true,
   // https://www.serverless.com/plugins/serverless-nextjs-plugin
-  env: {
-    BUCKET_NAME,
-    WEB_CF_DISTRIBUTION_ID,
-  },
   webpack5: false,
   webpack: (config, options) => {
     // alias 추가
