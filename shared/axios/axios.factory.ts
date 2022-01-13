1b1a648ae9d540bdd16276255471c7028a6437b9
jtpark <jtpark@deleo.co.kr>
2022-01-13 18:30:06 +0900

    feat(axios): axios camelcase - snakecase converter 추가 client와 server간 variables format이 상이하여 이를 맞추기 위해 converter를 추가함 - axios interceptor에서 변수 변환 - model 키값 camelCase로 변경
    ref #24

--- shared/axios/axios.factory.ts
+++ shared/axios/axios.factory.ts
@@ -1,6 +1,55 @@
-import axios from 'axios';
+import axios, { AxiosResponse, AxiosRequestConfig, AxiosError } from 'axios';
 import { API_URL } from '~/shared/constants/environments';
+import qs from 'qs';
+import { camelizeKeys, decamelizeKeys } from 'humps';
 
-const axiosInstance = axios.create({ baseURL: API_URL });
+/**
+ * @see https://github.com/axios/axios
+ */
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-type': 'application/json; charset=utf-8',
+  },
+  paramsSerializer: function (params) {
+    return qs.stringify(params, { arrayFormat: 'brackets' });
+  },
+});
+
+/**
+ * @see https://javascript.plainenglish.io/configuring-a-camelcase-to-snake-case-parser-with-axios-9fa34fd3b16f
+ */
+function axiosRequestInterceptor(config: AxiosRequestConfig) {
+  if (config.params) {
+    config.params = decamelizeKeys(config.params);
+  }
+  if (config.data) {
+    config.data = decamelizeKeys(config.data);
+  }
+  return config;
+}
+
+function axiosResponseInterceptor(response: AxiosResponse) {
+  if (
+    response.data &&
+    response.headers['content-type'] === 'application/json'
+  ) {
+    response.data = camelizeKeys(response.data);
+  }
+  return response;
+}
+
+function axiosErrorInterceptor(error: AxiosError) {
+  return Promise.reject(error);
+}
+
+axiosInstance.interceptors.request.use(
+  axiosRequestInterceptor,
+  axiosErrorInterceptor,
+);
+axiosInstance.interceptors.response.use(
+  axiosResponseInterceptor,
+  axiosErrorInterceptor,
+);
 
 export default axiosInstance;
