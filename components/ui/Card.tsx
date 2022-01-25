b47b608878fbf7bdb8eb0ab03348799683cbc135
akariyan <fntlsltm@gmail.com>
2022-01-25 22:16:27 +0900

    feat(Main): 검색창 개발 - main 페이지를 index 페이지로 사용 - Header에 ui-kits 업데이트에 따른 AppBar 추가 - 검색 이벤트 추가 ref #28

--- components/ui/Card.tsx
+++
@@ -1,9 +0,0 @@
-import { styled } from '../../stitches.config';
-
-const StyledCard = styled('div', {});
-
-const Card = function () {
-  return <StyledCard></StyledCard>;
-};
-
-export default Card;
