fcd505291a01ae69dc7afa400b244459646da8bb
akariyan <fntlsltm@gmail.com>
2022-01-27 19:33:00 +0900

    feat(Main): CardList 분리 - CardList를 분리하여 내부에서 Card를 map function을 사용해 rendering ref #28

---
+++ components/index/CardList.tsx
@@ -0,0 +1,40 @@
+import { Box } from '@nolmungshemung/ui-kits';
+import { Contents } from '~/data/services/services.model';
+import { styled } from '../../stitches.config';
+import Card from './Card';
+
+const StyledCardList = styled(Box, {
+  gridArea: 'result',
+  display: 'grid',
+  gridTemplateColumns: 'repeat(4, 1fr)',
+  gridTemplateRows: 'repeat(5, 1fr)',
+  columnGap: '1.5rem',
+  rowGap: '0.875rem',
+});
+
+interface CardListProps {
+  resultList: Contents[];
+}
+
+const CardList = function ({ resultList }: CardListProps) {
+  console.log(resultList);
+  return (
+    <StyledCardList>
+      {/* TO-DO : Writer 대응 */}
+      {resultList.map((search: Contents) => (
+        <Card
+          key={search.contentsId}
+          contentsId={search.contentsId}
+          title={search.title}
+          thumbnail={search.thumbnail}
+          introduction={search.introduction}
+          isTranslate={search.isTranslate}
+          language={search.language}
+          writer={search.writer}
+        />
+      ))}
+    </StyledCardList>
+  );
+};
+
+export default CardList;
