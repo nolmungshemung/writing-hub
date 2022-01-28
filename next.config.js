80627293d94ca0b59df17066b01dbb6fec1f28a9
jtpark <jtpark@deleo.co.kr>
2022-01-28 10:54:15 +0900

    feat: nextjs upgrade to v12 nextjs 개발, 빌드 성능 향상을 위해 버전을 12로 업그레이드함
    ref #31

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   reactStrictMode: true,
+  swcMinify: true,
   // https://www.serverless.com/plugins/serverless-nextjs-plugin
-  webpack5: false,
   webpack: (config, options) => {
     // alias 추가
-    config.resolve.alias["~"] = path.resolve(__dirname, "src");
+    config.resolve.alias['~'] = path.resolve(__dirname, 'src');
 
     return config;
   },
