4027ddf0130e513b242b457536ff2c06f53c3df4
jtpark <vigabhan1231@gmail.com>
2022-02-12 18:07:54 +0900

    feat: main writers mock api 수정 swagger api 에 맞춰 mock api response를 변경함
    ref #39

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,7 +1,11 @@
 import { rest, RestRequest } from 'msw';
 import { WritingContentsRequest } from '~/data/services/services.model';
 import { API_URL } from '~/shared/constants/environments';
-import { ContentsFactory, ErrorResponseFactory } from './factories';
+import {
+  ContentsFactory,
+  ErrorResponseFactory,
+  WriterFactory,
+} from './factories';
 
 export const handlers = [
   rest.get(`${API_URL}/services/main_contents`, (req, res, ctx) => {
@@ -29,24 +33,28 @@ export const handlers = [
     }
   }),
   rest.get(`${API_URL}/services/main_writers`, (req, res, ctx) => {
-    return res(
-      ctx.json({
-        status_code: 200,
-        msg: '응답 성공',
-        data: {
-          main_writer_list: [
-            {
-              writer_name: '장발장',
-              writer_id: '10asff',
-            },
-            {
-              writer_name: '어제의 나',
-              writer_id: '10asfg',
-            },
-          ],
-        },
-      }),
-    );
+    const keyword = req.url.searchParams.get('keyword');
+
+    if (keyword !== 'invalid') {
+      const start = Number(req.url.searchParams.get('start'));
+      const count = Number(req.url.searchParams.get('count'));
+      const writers = WriterFactory.buildList(count);
+
+      return res(
+        ctx.status(200),
+        ctx.json({
+          msg: 'success!',
+          data: {
+            main_writer_list: writers,
+            is_last: false,
+            start,
+          },
+        }),
+      );
+    } else {
+      const errorResponse = ErrorResponseFactory.build();
+      return res(ctx.status(422), ctx.json(errorResponse));
+    }
   }),
   rest.get(`${API_URL}/services/reading_contents`, (req, res, ctx) => {
     return res(
