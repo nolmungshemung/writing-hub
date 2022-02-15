db4a022bf677b3d8cd38dbd250f9acead41fa82f
jtpark <vigabhan1231@gmail.com>
2022-02-15 16:16:43 +0900

    refactor: 404 code 추가 및 react-virtual 제거 react-virtual를 사용했을 때 response개수가 홀수이면 문제가 있어서 writers와 동일하게 적용함 404 code가 추가되어 mock api에 적용함 - 화면에서 에러처리 추가 필요
    close #56

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, Fragment } from 'react';
+import { useState, useRef } from 'react';
 import { NextPage } from 'next';
 import { dehydrate, QueryClient } from 'react-query';
 import Router from 'next/router';
@@ -13,11 +13,7 @@ import { useInfinityContents } from '~/data/services/services.hooks';
 import { SuccessResponse } from '~/shared/types';
 import Card from '~/components/Main/Card';
 import { SkeletonCard } from '~/components/Skeleton';
-import {
-  DEFAULT_SEARCH_RANGE,
-  GRID_COLUMN_COUNT,
-} from '~/shared/constants/pagination';
-import { useVirtual } from 'react-virtual';
+import { DEFAULT_SEARCH_RANGE } from '~/shared/constants/pagination';
 import { NextSeo } from 'next-seo';
 
 const StyledMain = styled('div', {
@@ -53,7 +49,6 @@ const initialState: ContentsSearchParams = {
 
 const Main: NextPage = function () {
   const loadMoreRef = useRef<HTMLDivElement>(null);
-  const parentRef = useRef<HTMLDivElement>(null);
 
   const [searchParams, setSearchParams] =
     useState<ContentsSearchParams>(initialState);
@@ -84,23 +79,6 @@ const Main: NextPage = function () {
     .map((page) => page.data.mainContentsList.map((contents) => contents))
     .flat();
 
-  /**
-   * @see https://react-virtual.tanstack.com/docs/overview
-   * @see https://codesandbox.io/s/github/tannerlinsley/react-virtual/tree/main/examples/variable?file=/src/main.jsx:4250-4350
-   */
-  const rowVirtualizer = useVirtual({
-    size: flatMainContents.length / GRID_COLUMN_COUNT,
-    parentRef,
-    overscan: 5,
-  });
-
-  const columnVirtualizer = useVirtual({
-    horizontal: true,
-    size: GRID_COLUMN_COUNT,
-    parentRef,
-    overscan: 5,
-  });
-
   useIntersectionObserver({
     target: loadMoreRef,
     enabled: hasNextPage,
@@ -125,28 +103,14 @@ const Main: NextPage = function () {
     }
 
     if (pages.length > 0) {
-      let cardIndex = 0;
-
       return (
         <>
-          <NextSeo
-            title="WritingHub: 작품 검색"
-            description="라이팅허브 작품 검색 화면"
-          />
-          {rowVirtualizer.virtualItems.map((virtualRow) => (
-            <Fragment key={virtualRow.index}>
-              {columnVirtualizer.virtualItems.map((virtualColumn) => {
-                const contents = flatMainContents[cardIndex++];
-
-                return (
-                  <Card
-                    {...contents}
-                    key={contents.contentsId + virtualColumn.index}
-                    onCardClick={() => onCardClick(contents.contentsId)}
-                  />
-                );
-              })}
-            </Fragment>
+          {flatMainContents.map((contents) => (
+            <Card
+              key={contents.contentsId}
+              {...contents}
+              onCardClick={() => onCardClick(contents.contentsId)}
+            />
           ))}
           {isFetchingNextPage && <SkeletonCard />}
         </>
@@ -157,52 +121,53 @@ const Main: NextPage = function () {
   };
 
   return (
-    <StyledMain>
-      <SytledTopArea>
-        <Box
-          css={{
-            position: 'relative',
-            display: 'flex',
-            marginTop: '$sp-50',
-            justifyContent: 'center',
-            width: '1216px',
-          }}
-        >
-          <Search
-            placeholder="검색어를 입력해주세요."
-            onChange={onChange}
-            onEnter={onEnter}
-            onSearch={onSearch}
-          />
-          <Button
-            color="white"
-            outline="black"
+    <>
+      <NextSeo
+        title="WritingHub: 작품 검색"
+        description="라이팅허브 작품 검색 화면"
+      />
+      <StyledMain>
+        <SytledTopArea>
+          <Box
             css={{
-              position: 'absolute',
-              right: '0',
-              cursor: 'pointer',
+              position: 'relative',
+              display: 'flex',
+              marginTop: '$sp-50',
+              justifyContent: 'center',
+              width: '1216px',
             }}
-            onClick={onWriterSearchButtonClick}
           >
-            작가검색
-          </Button>
-        </Box>
-      </SytledTopArea>
-      <StyledCardList
-        ref={parentRef}
-        css={{
-          height: `${rowVirtualizer.totalSize}px`,
-        }}
-      >
-        {renderCardList()}
-        <Box
-          ref={loadMoreRef}
-          css={{
-            height: '$height-md',
-          }}
-        />
-      </StyledCardList>
-    </StyledMain>
+            <Search
+              placeholder="검색어를 입력해주세요."
+              onChange={onChange}
+              onEnter={onEnter}
+              onSearch={onSearch}
+            />
+            <Button
+              color="white"
+              outline="black"
+              css={{
+                position: 'absolute',
+                right: '0',
+                cursor: 'pointer',
+              }}
+              onClick={onWriterSearchButtonClick}
+            >
+              작가검색
+            </Button>
+          </Box>
+        </SytledTopArea>
+        <StyledCardList>
+          {renderCardList()}
+          <Box
+            ref={loadMoreRef}
+            css={{
+              height: '$height-md',
+            }}
+          />
+        </StyledCardList>
+      </StyledMain>
+    </>
   );
 };
 
