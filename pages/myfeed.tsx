9602024a92688e58d620e2214b491f19435a5bf6
akariyan <fntlsltm@gmail.com>
2022-02-16 10:27:52 +0900

    feat(Feed): MyFeed Page Init Ref #54

---
+++ pages/myfeed.tsx
@@ -0,0 +1,14 @@
+import { NextSeo } from 'next-seo';
+
+const MyFeed = function () {
+  return (
+    <>
+      <NextSeo
+        title="WritingHub: 내 피드"
+        description="라이팅허브 내 피드 화면"
+      />
+    </>
+  );
+};
+
+export default MyFeed;
